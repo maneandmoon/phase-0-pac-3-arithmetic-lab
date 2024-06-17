efa7a085bd355164979b302c2d84ce1564fbd371
LindaTa <lindata@maneandmoon.com>
2024-06-17 11:08:14 -0500

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+    return a + b;
+  }
+
+function subtract(a, b) {
+return a - b;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+
+function increment(a) {
+return a += 1;
+
+}
+
+function decrement(a) {
+    return a -= 1;
+
+}
+
+
+    
+    function makeInt(string){
+    
+    return parseInt(string,10)
+    return parseInt(NaN);
+
+}
+    
+
+function preserveDecimal(a) {
+    return parseFloat(a);
+    return parseFloat(NaN);
+
+}
